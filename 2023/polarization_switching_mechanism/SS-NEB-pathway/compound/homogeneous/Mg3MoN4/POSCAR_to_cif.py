6e8aaa9010133ca03b37c9d518e6844e02adf25d
Cheng-Wei Lee <clee2@Cheng-Weis-MacBook-Pro.local>
2024-02-05 16:24:55 -0700

    finished rotating the cells for SS-NEB pathway of compounds

--- 2023/polarization_switching_mechanism/SS-NEB-pathway/compound/homogeneous/Mg3MoN4/POSCAR_to_cif.py
+++
@@ -1,7 +0,0 @@
-from glob import iglob
-from pymatgen.core import Lattice, Structure
-
-
-for name in iglob('POSCAR-*.vasp'):
-    strc = Structure.from_file(name)
-    strc.to(filename=name.replace(".vasp",".cif"))
